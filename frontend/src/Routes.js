4d98fe1248f64d3865c7eab3757a746c82bf3b07
Koushik Dey <koushik.dey@live.com>
2022-12-13 17:46:23 +0200

    created subnav

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes as AllRoutes } from 'react-router-dom';
+import SubNav from './components/common/SubNav';
 import Navbar from './components/common/Navbar';
 import Journey from './components/journey/Journey';
 import Station from './components/station/Station';
@@ -8,6 +9,7 @@ import Home from './pages/Home';
 const Routes = () => {
     return (
         <BrowserRouter>
+            <SubNav />
             <Navbar />
               <AllRoutes>
                  <Route path="/" element={<Home />} />
--- frontend/src/components/common/SubNav.js
+++ frontend/src/components/common/SubNav.js
@@ -3,7 +3,33 @@ import './SubNav.scss';
 
 const SubNav = () => {
   return (
-    <div>SubNav</div>
+    <header className="header-area">
+        <div className="header-top">
+            <div className="container-fluid">
+                <div className="top_nav_area">
+                    <div className="col-lg-6">
+                        <div className="header_top_left_side">
+                            <ul>
+                                <li><a href="#/">DUMMY1</a></li>
+                                <li><a href="#/">DUMMY2</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="header-right-social text-center text-sm-right">
+                            <ul>
+                                <li><a href="https://www.facebook.com//"><i className="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://twitter.com/"><i className="fab fa-twitter"></i></a></li>
+                                <li><a href="https://github.com/koushik80/"><i className="fab fa-github"></i></a></li>
+                                <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="https://www.youtube.com/"><i className="fab fa-youtube"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 
