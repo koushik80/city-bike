0e022b325b8325830d2fc17471b1a22c8df3be81
Koushik Dey <koushik.dey@live.com>
2022-12-13 19:16:06 +0200

    :construction: working on station component
    on going..

--- frontend/src/components/station/Station.js
+++ frontend/src/components/station/Station.js
@@ -1,11 +1,54 @@
+import { Container, Grid } from '@mui/material';
 import React from 'react';
+import { useGetStationsQuery } from '../../features/station/stationApi';
 import './Station.scss';
 
 const Station = () => {
-  return (
-    <div>
 
-    </div>
+   // const [page, setPage] = React.useState(1);
+    // const handleChange = (event, value) => {
+    //   setPage(value);
+    // };
+
+  const { data, isLoading, isError, error } = useGetStationsQuery();
+
+  // thinking what can be displayed
+
+  let content;
+
+  if (isLoading) {
+    content = [...new Array(30)].map((item, i) => (
+      <h1 key={i}></h1>
+    ))
+  };
+
+  if (!isLoading && isError) content = <div>{error?.message}</div>
+
+  if (!isLoading && !isError && data?.stations?.length === 0) content = <div>data not found...</div>
+
+  if (!isLoading && !isError && data?.stations?.length !== 0) {
+    content = data?.stations?.map((station, i) => (
+          <p key={station?._id} item={station}></p>
+        ))
+    }
+
+  return (
+     <Container>
+        <div className='station-page'>
+            <div className='station-page--search-section'>
+                <form action="">
+                    <div className='station-page--search-section--div'>
+                      <input type="text" placeholder='search by station name' />
+                      <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+            </div>
+            <Grid container spacing={2}>
+              {content}
+            </Grid>
+             {/* <Pagination count={10} variant="outlined" color="primary"  page={page} onChange={handleChange} /> */}
+        </div>
+     </Container>
   );
 };
 
