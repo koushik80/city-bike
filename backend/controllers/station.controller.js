afbf455b45b684f1d0e73c6a2219c3e1d1372226
Koushik Dey <koushik.dey@live.com>
2022-12-14 14:30:51 +0200

    station search and pagination features added to the backend

--- backend/controllers/station.controller.js
+++ backend/controllers/station.controller.js
@@ -2,14 +2,18 @@
 const ErrorHandler = require('../error/errorHandler');
 const catchAsyncError = require('../middleware/catchAsyncError');
 const Station = require('./../models/station.model');
+const ApiFeatures = require('../utils/ApiFeatures');
 
 // GET ALL STATIONS
 exports.getAllStations = catchAsyncError( async (req, res, next) => {
 
-    const stations = await Station.find();
+    const apiFeature = new ApiFeatures(Station.find(), req.query)
+    .search()
+    .pagination(100);
+    const stations = await apiFeature.query;
     const totalStations = await Station.countDocuments();
 
-    if(stations?.length === 0) return next(new ErrorHandler("Station data not found", 404));
+    if (stations?.length === 0) return next(new ErrorHandler("Station data not found", 404));
 
     res.status(200).json({
         success: true,
