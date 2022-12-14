9a719e9f97a5716d183f6d24990217db3ce81826
Koushik Dey <koushik.dey@live.com>
2022-12-14 13:58:51 +0200

    created api-features for search and pagination

---
+++ backend/utils/ApiFeatures.js
@@ -0,0 +1,31 @@
+class ApiFeatures {
+
+    constructor(query, queryStr) {
+        this.query = query;
+        this.queryStr = queryStr;
+    }
+
+    search() {
+        const keyword = this.queryStr.keyword ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: "i"
+            }
+        } : {};
+        this.query = this.query.find({...keyword});
+
+        return this;
+    }
+
+    pagination(resultPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+
+        const skip = resultPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resultPerPage).skip(skip);
+
+        return this;
+    }
+}
+
+module.exports = ApiFeatures;
